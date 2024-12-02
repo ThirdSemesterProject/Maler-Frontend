480f356ff1c90307711287f142539c89032b6c68
Oskar Eriksen <oska1001@stud.kea.dk>
2024-12-02 11:00:38 +0100

    index.html footerScript.js

---
+++ JS/footer.js
@@ -0,0 +1,52 @@
+export function loadFooter() {
+    const existingFooter = document.querySelector('.footer');
+    if (existingFooter) {
+        existingFooter.remove(); // Fjern eksisterende footer, hvis den findes
+    }
+
+    const footer = `
+        <footer class="footer bg-white py-4 shadow-md">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col">
+                        <h4>FIRMAOPLYSNINGER</h4>
+                        <ul>
+                            <li><a href="/">Maling.dk</a></li>
+                            <li><a href="/">Ring til os på</a></li>
+                            <li><a href="/">Skriv til os på mail</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>INFORMATION</h4>
+                        <ul>
+                            <li><a href="/">Om os</a></li>
+                            <li><a href="/">Handelsbetingelser</a></li>
+                            <li><a href="/">###</a></li>
+                            <li><a href="/">###</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>Hjælp & kontakt</h4>
+                        <ul>
+                            <li><a href="/">Kontakt os</a></li>
+                            <li><a href="/">Hyppige spørgsmål</a></li>
+                            <li><a href="/">Mere information</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>Følg os på!</h4>
+                        <div class="social-links">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <p>© Maling.dk - 2024. All Rights Reserved</p>
+            </div>
+        </footer>
+    `;
+    document.body.insertAdjacentHTML('beforeend', footer);
+}
